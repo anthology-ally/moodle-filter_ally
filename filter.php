82bdf9ce1dfd203efc25bc3c9136940576c87304
gthomas2 <brudinie@gmail.com>
2018-11-16 11:02:49 +0000

    INT-13900: Enhance behat tests and fix bugs discovered by new tests

--- filter.php
+++ filter.php
@@ -423,6 +423,16 @@ EOF;
             $params = new stdClass();
             if (strpos($PAGE->pagetype, 'mod-lesson') !== false) {
                 $pageid = optional_param('pageid', null, PARAM_INT);
+                if ($pageid === null) {
+                    $cmid = optional_param('id', null, PARAM_INT);
+                    if ($cmid) {
+                        list ($course, $cm) = get_course_and_cm_from_cmid($cmid);
+                        $lessonid = $cm->instance;
+                        // Get first page id for lesson.
+                        $sql = 'SELECT min(id) FROM {lesson_pages} WHERE lessonid = ?';
+                        $pageid = $DB->get_field_sql($sql, [$lessonid]);
+                    }
+                }
                 $answerid = optional_param('answerid', null, PARAM_INT);
                 $params->answerid = $answerid;
                 $params->pageid = $pageid;
@@ -434,14 +444,14 @@ EOF;
                     if ($cmid) {
                         list ($course, $cm) = get_course_and_cm_from_cmid($cmid);
                         $bookid = $cm->instance;
-                        // Get first chapter id for book
+                        // Get first chapter id for book.
                         $sql = 'SELECT min(id) FROM {book_chapters} WHERE bookid = ?';
                         $chapterid = $DB->get_field_sql($sql, [$bookid]);
                     }
                 }
                 $params->chapterid = $chapterid;
             }
-            
+
             $amdargs = [$jwt, $configvars, $canviewfeedback, $candownload, $COURSE->id, $params];
             $page->requires->js_call_amd('filter_ally/main', 'init', $amdargs);
             $jsinitialised = true;
