open-lms-open-source/moodle-mod_collaborate

INT-17886: Fix code checker 3.1.0

tests/jwthelper_test.php
@@ -22,13 +22,13 @@
  * @copyright Copyright (c) 2017 Open LMS
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
+namespace mod_collaborate;
 defined('MOODLE_INTERNAL') || die();
 global $CFG, $DB;
 
 use mod_collaborate\rest\jwthelper;
 
-class mod_collaborate_jwthelper_testcase extends advanced_testcase {
+class jwthelper_test extends \advanced_testcase {
     public function test_get_token() {
         $token = jwthelper::get_token('mykey', 'mysecret');
         $this->assertNotEmpty($token);

anthology-ally/moodle-filter_ally

tests/jwthelper_test.php
@@ -13,10 +13,9 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+namespace filter_ally;
 
-defined('MOODLE_INTERNAL') || die();
-
-class filter_ally_jwthelper_testcase extends advanced_testcase {
+class jwthelper_test extends \advanced_testcase {
 
     protected function config_set_ok() {
         set_config('secret', 'WAzk9ohDeK', 'tool_ally');