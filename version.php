9c1a1853773a1d43dc8ddaf409d6613597f1a960
Julian Tovar <julian.tovar@openlms.net>
2022-06-06 09:28:47 -0500

    INT-18027: 3.11 MP3 Version bump
    (cherry picked from commit a7e00d3756620bafe76cb00882415021704a27f2)

--- version.php
+++ version.php
@@ -27,10 +27,10 @@ defined('MOODLE_INTERNAL') || die();
 /** @var stdClass $plugin */
 $plugin->component = 'filter_ally';
 $plugin->release   = '3.11.3';
-$plugin->version   = 2022041300;
+$plugin->version   = 2022060600;
 $plugin->requires  = 2021051700;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = [
-    'tool_ally'      => 2022041300,
-    'report_allylti' => 2022041300,
+    'tool_ally'      => 2022060600,
+    'report_allylti' => 2022060600,
 ];
