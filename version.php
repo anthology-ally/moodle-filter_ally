f9cb92de88a780890492e7eb7e91bda040e468c0
Rafael Monterroza <Rafael.Monterroza@openlms.net>
2022-10-18 18:09:38 -0500

    INT-18447: Bumping Ally versions in 4.0 branch

--- version.php
+++ version.php
@@ -26,11 +26,11 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'filter_ally';
-$plugin->release   = '3.11.6';
-$plugin->version   = 2022101200;
+$plugin->release   = '4.0.1';
+$plugin->version   = 2022101800;
 $plugin->requires  = 2021051700;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = [
-    'tool_ally'      => 2022101200,
-    'report_allylti' => 2022101200,
+    'tool_ally'      => 2022101800,
+    'report_allylti' => 2022101800,
 ];
