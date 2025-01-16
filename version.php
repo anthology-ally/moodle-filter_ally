ebd61cf435ffe9657d12f3645e192c3dc3ad69ee
gthomas2 <brudinie@gmail.com>
2025-01-16 13:17:38 +0000

    Moodle 404 stable (#76)
    * ALLY-14: Moodle 404 initial version
    
    * ALLY-14: Fix style bugs introduced with MOODLE 4.4
    
    * ALLY-14: Adjust workflow to use citricity versions of openlms plugins for compatibility reasons

--- version.php
+++ version.php
@@ -26,11 +26,11 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'filter_ally';
-$plugin->release   = '4.3.6';
-$plugin->version   = 2024082300;
-$plugin->requires  = 2022041900;
+$plugin->release   = '4.4.3';
+$plugin->version   = 2024112200;
+$plugin->requires  = 2024042203;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = [
-    'tool_ally'      => 2024060300,
-    'report_allylti' => 2024060300,
+    'tool_ally'      => 2024112200,
+    'report_allylti' => 2024112200
 ];
