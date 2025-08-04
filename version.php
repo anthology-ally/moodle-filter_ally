ea5b67ef525b69eb2b6e5e6da754f38f3011a845
gthomas2 <brudinie@gmail.com>
2025-08-04 18:00:36 +0200

    Moodle 500 wip [skip-openlms] (#94)
    * ALLY-27: Allow open lms tests to be skipped
    
    * ALLY-27: Skip hsuforum tests if hsuforum not installed
    
    * ALLY-27: Convert main.js to es6 module
    
    * ALLY-28: Prevent wrapping of file anchors where the anchor simply contains an image with the same source
    
    * ALLY-31: Ensure image file is wrapped on upload
    
    * ALLY-31: Fix bug with section annotations. Fix unit tests
    ALLY-27: Moodle 5.0 version.php update
    [skip-openlms]

--- version.php
+++ version.php
@@ -25,12 +25,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
-$plugin->component = 'filter_ally';
-$plugin->release   = '4.5.0';
-$plugin->version   = 2025050300;
-$plugin->requires  = 2024092700;
-$plugin->maturity  = MATURITY_STABLE;
+$plugin->component    = 'filter_ally';
+$plugin->release      = '5.0.0';
+$plugin->version      = 2025072400;
+$plugin->requires     = 2025041400;
+$plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = [
-    'tool_ally'      => 2025032300,
-    'report_allylti' => 2025032300,
+    'tool_ally'      => 2025072400,
+    'report_allylti' => 2025072400,
 ];
