ea2183a259950e54830d2f2e7705e5528cc36bd0
Andrew Moore <andrew.moore@anthology.com>
2026-03-25 16:25:59 -0400

    Change to support only Moodle 4.5

--- version.php
+++ version.php
@@ -36,9 +36,13 @@ $plugin->dependencies = [
     'report_allylti' => 2026012850,
 =======
 $plugin->release      = '4.5.1';
-$plugin->version      = 2026012800;
+$plugin->version      = 2026032500;
 $plugin->requires     = 2024100700;
 $plugin->maturity     = MATURITY_STABLE;
+$plugin->supported    = [
+    405,
+    405,
+];
 $plugin->dependencies = [
     'tool_ally'      => 2026012800,
     'report_allylti' => 2026012800,
