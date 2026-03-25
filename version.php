6d308e11c3a810739bf914c513eab72bc2ed10ef
Andrew Moore <andrew.moore@anthology.com>
2026-03-25 16:19:41 -0400

    Update to support only Moodle 5.0

--- version.php
+++ version.php
@@ -27,9 +27,13 @@ defined('MOODLE_INTERNAL') || die();
 /** @var stdClass $plugin */
 $plugin->component    = 'filter_ally';
 $plugin->release      = '5.0.1';
-$plugin->version      = 2026012825; // Sub version is bumped to 25 to avoid conflicts with backports in MOODLE_405.
+$plugin->version      = 2026032525; // Sub version is bumped to 25 to avoid conflicts with backports in MOODLE_405.
 $plugin->requires     = 2025041400;
 $plugin->maturity     = MATURITY_STABLE;
+$plugin->supported    = [
+    500,
+    500,
+];
 $plugin->dependencies = [
     'tool_ally'      => 2026012825,
     'report_allylti' => 2026012825,
