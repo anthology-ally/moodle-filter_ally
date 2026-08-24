f25f613e0bdd27b39e1a2bc947da6d5374272d26
Guy Thomas <dev@citri.city>
2026-08-24 10:00:14 +0100

    ALLY-43: Fix position of feedback marker

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 /** @var stdClass $plugin */
 $plugin->component    = 'filter_ally';
 $plugin->release      = '5.2.0';
-$plugin->version      = 2026070600;
+$plugin->version      = 2026082400;
 $plugin->requires     = 2025041400;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = [
